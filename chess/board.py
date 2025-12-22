d2bc427ee1895cd8823fa6127d9c79f3e25c90c5
Hadi <hadialissa1021@gmail.com>
2025-12-22 23:33:54 +0300

    Add basic move function and display board

--- chess/board.py
+++ chess/board.py
@@ -1,3 +1,4 @@
+import os
 class Board:
     def __init__(self):
         self.board = self._create_board()
@@ -25,13 +26,20 @@ class Board:
         print("    a b c d e f g h\n")
     # لتحريك قطعة بدون القواعد (حاليا)
     def move(self, start, end):
+        # خريطة لمساواة بين الحرف و رقم لسهولة تحويله لاحداثيات عامود
         col_map = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
+        # اخذ اول حرف من المدخل و وضعه في الخريطة لنحصل على الرفم المقابل له
         start_col = col_map[start[0]]
+        # لعكس الموقع نطرح من رقم سبعة الرقم المدخل و نزيد واحد نحصل على الصف
         start_row = 8 - int(start[1])
         end_col = col_map[end[0]]
         end_row = 8 - int(end[1])
-
-
-
+        # نضع مكان الوجهة ما كان موجود في مكان البداية و مكان البداية نضع نقطة
+        self.board[end_row][end_col] = self.board[start_row][start_col]
+        self.board[start_row][start_col] = "."
 b = Board()
+b.display()
+start = input("Enter the starting position: ").lower()
+end = input("Enter the ending position: ").lower()
+b.move(start, end)
 b.display()
